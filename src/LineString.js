fc959d96f80943e9ed6dddbd4534c8d6e0d3d75c
Steven Miyakawa <steven.miyakawa@here.com>
2016-07-22 12:06:00 -0700

    Create class to provide template for linestrings

---
+++ src/LineString.js
@@ -0,0 +1,15 @@
+export default class {
+
+  constructor({ coordinates = [], styles }) {
+    const defaultStyles = {
+      color: 'black',
+      lineWidth: 2
+    };
+    const options = {
+      coordinates,
+      ...{...defaultStyles, styles}
+    };
+
+    Object.assign(this, options);
+  }
+};
